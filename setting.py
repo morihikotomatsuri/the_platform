8e85ef4d05baeeb982a0575469245e7d4ffc1ba6
morihikotomatsuri <morihiko.toma@gmail.com>
2024-07-13 16:05:47 +0900

    update

--- setting.py
+++ setting.py
@@ -4,11 +4,12 @@
 """
 variables:        
     floors int:
-        floor_num is the size of HOLE list. 
-    food_amounts int:
-        food_amounts is the initial food amount.
+        the size of HOLE list. 
+    experimental_food_range int:
+        the initial food amount.
+    number_of_rounds
 """
 
-floors= 1000
+floors = 50
 experimental_food_range = range(0, 100, 10)
-number_of_rounds=100
+number_of_rounds = 50
